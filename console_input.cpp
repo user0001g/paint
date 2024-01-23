7bc5ed98dd41c8cb850449c5a21f70c05bf53a72
user0001g <152640262+user0001g@users.noreply.github.com>
2024-01-23 05:47:50 -0800

    Update console_input.cpp

--- console_input.cpp
+++ console_input.cpp
@@ -1,41 +1,45 @@
 #include "console_input.hpp"
 
+ConsoleInput::ConsoleInput() {
+	hInput = GetStdHandle(STD_INPUT_HANDLE);
+}
 
-HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
-INPUT_RECORD inputBuffer;
-DWORD eventsRead;
+ConsoleInput& ConsoleInput::getInput() {
+    static ConsoleInput instance;
+    return instance;
+}
 
-void readInput(){
+void ConsoleInput::readInput() {
 	ReadConsoleInput(hInput, &inputBuffer, 1, &eventsRead);
 }
 
-bool mouseLeftClickHeld() {
+bool ConsoleInput::mouseLeftClickHeld() {
 	return (inputBuffer.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED);
 }
 
-bool mouseMoved(){
+bool ConsoleInput::mouseMoved() {
 	return (inputBuffer.Event.MouseEvent.dwEventFlags == MOUSE_MOVED);
 }
 
-COORD getMousePosition(){
+COORD ConsoleInput::getMousePosition() {
 	return (inputBuffer.Event.MouseEvent.dwMousePosition);
 }
 
-char keyPressed(){
+char ConsoleInput::keyPressed() {
 	wchar_t ch = inputBuffer.Event.KeyEvent.uChar.UnicodeChar;
-	if (inputBuffer.Event.KeyEvent.bKeyDown){
+	if (inputBuffer.Event.KeyEvent.bKeyDown) {
 		ch = inputBuffer.Event.KeyEvent.uChar.UnicodeChar;
 		return (char(ch));
 	}
-	
+
 	return 'N';
 
 }
 
-bool ctrlHeld(){
+bool ConsoleInput::ctrlHeld() {
 	return (inputBuffer.Event.KeyEvent.dwControlKeyState == LEFT_CTRL_PRESSED);
 }
 
-bool consoleResized(){
+bool ConsoleInput::consoleResized() {
 	return (inputBuffer.EventType == WINDOW_BUFFER_SIZE_EVENT);
 }

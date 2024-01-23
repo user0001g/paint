3cabba37f1e4fcaa5fa26a3d0ee45cccfa8ceaf8
user0001g <152640262+user0001g@users.noreply.github.com>
2024-01-23 05:48:49 -0800

    Update window.hpp

--- window.hpp
+++ window.hpp
@@ -4,43 +4,45 @@
 #include <ncursesw/curses.h>
 #include <unistd.h>
 #include <windows.h>
+#include "console_input.hpp"
 
 class Window {
 	public:
-		Window(int rows, int cols);
+		Window(int rows, int cols, int posY = 0, int posX = 0, bool BoxFlag = false, int BorderFlag = 0, int fillFlag = 0, std::string textFlag = "");
 
-		bool isDrawing;
+		static bool isDrawing;
 
 		void setSize(int rows, int cols);
-		void setPosition(int rows, int cols);
+		void setPosition(int rows = 0, int cols = 0);
 		
 		void drawBox();
 		void drawBorder(int pair_value);
 		void fill(int pair_value);
+		void highlight(int pair_value);
+		void type(std::string todo);
+		
 		
 		void update();
 		void clear();
 		void bringForward();
-		void erase();
 		
 		void addCh(std::string);
-		void removeCh(char);
 		void addStringAt(const char*, int y, int x, int pair_value);
-		void returnInput();
 
 		void loadSave(std::string saveFile);
 		void Save(std::string saveTo);
 
 		bool isEnclosed();
 
-		void enableDrawing();
+		void drawFlag();
 		void Draw(int color);
-		void disableDrawing();
 
 	private:
 		WINDOW* ncurses_win;
+		
+		ConsoleInput& Input;
+		std::string Text;
 
 		void draw_pixel(WINDOW* win, short int* x, short int* y, char brush, int brushSize);
 };
 
-extern int isDrawing;
